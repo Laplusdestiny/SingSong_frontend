772e085c726c9e5835b83a33932a19b61b1178fc
Laplusdestiny <laplusdestiny@laplusdestiny.com>
2024-05-12 04:43:11 +0900

    Added Navbar frame

--- app.vue
+++ app.vue
@@ -1,5 +1,20 @@
 <template>
-  <!-- <NuxtLayout> -->
-  <NuxtPage />
-  <!-- </NuxtLayout> -->
+  <v-app>
+    <v-app-bar color="primary">
+      <v-app-bar-title>
+        <v-btn icon="mdi-music" to="/"></v-btn>SingSong
+        <v-btn to="/about">
+          <v-icon icon="mdi-information" size="large"></v-icon>
+          <v-tooltip activator="parent" location="bottom">About</v-tooltip>
+        </v-btn>
+      </v-app-bar-title>
+      <template v-slot:append>
+        <v-btn icon="mdi-account-plus" to="/signup"></v-btn>
+        <v-btn icon="mdi-account" to="/login"></v-btn>
+      </template>
+    </v-app-bar>
+    <v-main>
+      <NuxtPage />
+    </v-main>
+  </v-app>
 </template>
