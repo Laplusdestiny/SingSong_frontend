39462e01d0b9a10b72b306762fdf81db10b16aeb
Laplusdestiny <laplusdestiny@laplusdestiny.com>
2024-06-09 16:47:07 +0900

    Made toppage and about page

--- app.vue
+++ app.vue
@@ -2,7 +2,10 @@
   <v-app>
     <v-app-bar color="primary">
       <v-app-bar-title>
-        <v-btn icon="mdi-music" to="/"></v-btn>SingSong
+        <v-btn to="/">
+          <v-icon icon="mdi-music"></v-icon>
+        </v-btn>
+        SingSong
         <v-btn to="/about">
           <v-icon icon="mdi-information" size="large"></v-icon>
           <v-tooltip activator="parent" location="bottom">About</v-tooltip>
@@ -10,12 +13,24 @@
       </v-app-bar-title>
       <template v-slot:append>
         <template v-if="isAuthenticated">
-          <v-btn icon="mdi-account" to="/mypage"></v-btn>
-          <v-btn icon="mdi-logout" @click="logout"></v-btn>
+          <v-btn to="/mypage">
+            <v-icon icon="mdi-account"></v-icon>
+            <v-tooltip activator="parent" location="bottom">My page</v-tooltip>
+          </v-btn>
+          <v-btn @click="logout">
+            <v-icon icon="mdi-logout"></v-icon>
+            <v-tooltip activator="parent" location="bottom">Logout</v-tooltip>
+          </v-btn>
         </template>
         <template v-else>
-          <v-btn icon="mdi-account-plus" to="/signup"></v-btn>
-          <v-btn icon="mdi-login" to="/login"></v-btn>
+          <v-btn to="/signup">
+            <v-icon icon="mdi-account-plus"></v-icon>
+            <v-tooltip activator="parent" location="bottom">Sign up</v-tooltip>
+          </v-btn>
+          <v-btn to="/login">
+            <v-icon icon="mdi-login"></v-icon>
+            <v-tooltip activator="parent" location="bottom">Login</v-tooltip>
+          </v-btn>
         </template>
       </template>
     </v-app-bar>
--- pages/index.vue
+++ pages/index.vue
@@ -1,3 +1,81 @@
 <template>
-    <div>Top page</div>
+    <div class="background-container">
+        <div class="overlay"></div>
+        <div class="hero">
+            <h1>あなたのカラオケライフをもっと楽しく、もっと便利に</h1>
+            <button>
+                <NuxtLink to="/about">詳しくはこちら</NuxtLink>
+            </button>
+        </div>
+    </div>
 </template>
+
+<script setup lang="ts">
+useHead({
+    title: 'SingSong - あなたのカラオケライフをもっと楽しく、もっと便利に'
+})
+</script>
+
+<style scoped>
+.background-container {
+    position: relative;
+    background-image: url('/static/background.webp');
+    background-size: cover;
+    background-position: center;
+    height: 100vh;
+    /* 画面の高さ全体 */
+    width: 100%;
+    /* 画面の幅全体 */
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    justify-content: center;
+    color: white;
+    /* 必要に応じてテキストの色を変更 */
+}
+
+.overlay {
+    position: absolute;
+    top: 0;
+    left: 0;
+    height: 100%;
+    width: 100%;
+    background-color: rgba(0, 0, 0, 0.6);
+    /* 透明度を調整 */
+    z-index: 1;
+}
+
+.background-container>*:not(.overlay) {
+    position: relative;
+    z-index: 2;
+}
+
+.hero {
+    text-align: center;
+    padding: 2rem;
+    /* 追加 */
+}
+
+.hero h1 {
+    font-size: 2.5rem;
+}
+
+.hero a {
+    color: white;
+}
+
+button {
+    display: inline-block;
+    padding: 1rem 2rem;
+    background-color: #007bff;
+    color: white;
+    border: none;
+    border-radius: 0.25rem;
+    cursor: pointer;
+    font-size: 1rem;
+}
+
+button:hover {
+    background-color: #0056b3;
+}
+</style>
